ff3be18e7c1fdb48ae37463e124a44cc6c5dcafb
Sana Mohiuddin <smohi002@ucr.edu>
2019-11-18 15:41:33 -0800

    Made factory base and made Add functionality work

---
+++ factory.hpp
@@ -0,0 +1,45 @@
+#include <string>
+
+Base* parse(char** input, int length) {
+		std::string expression = argv[1];
+		std::string parsedExpression = expression;
+		bool firstSign = false;
+		char firstSign1;
+		int firstSignIndex = 0;
+		int sizeToErase = 0;
+		for (int i = 0; i < expression.size(); i++) {
+			if ((expression.at(i) == '+' || expression.at(i) == '-' || expression.at(i) == '*' || expression.at(i) == '/') && firstSign == false) {
+				firstSign = true;
+				firstSign1 = expression.at(i);
+				std::cout << expression.at(i) << std::endl;
+				firstSignIndex = i;
+			}
+			else if ((expression.at(i) == '+' || expression.at(i) == '-' || expression.at(i) == '*' || expression.at(i) == '/') && firstSign == true) {
+				//std::string parsedExpression = expression;
+				sizeToErase = expression.size() - i;
+				parsedExpression.erase(i, sizeToErase);
+			}
+		}
+		std::cout << expression << std::endl;
+		std::cout << parsedExpression << std::endl;
+
+		std::string operator1 = "";
+		std::string operator2;
+		double oper1;
+		double oper2;
+		if (firstSign1 == '+') {
+			for (int i = 0; i < firstSignIndex; i++) {
+				operator1 += parsedExpression.at(i);
+				oper1 = std::atof(operator1.c_str());
+			}
+			for (int i = firstSignIndex + 1; i < parsedExpression.size(); i++) {
+				operator2 += parsedExpression.at(i);
+				oper2 = std::atof(operator2.c_str());
+				
+			}
+			Op* op1 = new Op(oper1);
+			Op* op2 = new Op(oper2);
+			Add* add = new Add(op1, op2);
+		}
+}
+
