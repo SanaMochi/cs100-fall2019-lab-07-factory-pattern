014fea9bd9886d9f015f28608e44e3f3793781bd
Sana Mohiuddin <smohi002@ucr.edu>
2019-11-18 16:07:29 -0800

    Make sub/mult/divFactories

--- calculator.cpp
+++ calculator.cpp
@@ -28,6 +28,66 @@ Add* addFactory(int firstSignIndex, std::string parsedExpression) {
 		return add;
 }
 
+Sub* subFactory(int firstSignIndex, std::string parsedExpression) {
+		std::string operator1 = "";
+		std::string operator2 = "";
+		double oper1;
+		double oper2;
+		for (int i = 0; i < firstSignIndex; i++) {
+			operator1 += parsedExpression.at(i);
+			oper1 = std::atof(operator1.c_str());
+		}
+		for (int i = firstSignIndex + 1; i < parsedExpression.size(); i++) {
+			operator2 += parsedExpression.at(i);
+			oper2 = std::atof(operator2.c_str());
+			
+		}
+		Op* op1 = new Op(oper1);
+		Op* op2 = new Op(oper2);
+		Sub* sub = new Sub(op1, op2);
+		return sub;
+}
+
+Mult* multFactory(int firstSignIndex, std::string parsedExpression) {
+		std::string operator1 = "";
+		std::string operator2 = "";
+		double oper1;
+		double oper2;
+		for (int i = 0; i < firstSignIndex; i++) {
+			operator1 += parsedExpression.at(i);
+			oper1 = std::atof(operator1.c_str());
+		}
+		for (int i = firstSignIndex + 1; i < parsedExpression.size(); i++) {
+			operator2 += parsedExpression.at(i);
+			oper2 = std::atof(operator2.c_str());
+			
+		}
+		Op* op1 = new Op(oper1);
+		Op* op2 = new Op(oper2);
+		Mult* mult = new Mult(op1, op2);
+		return mult;
+}
+
+Div* divFactory(int firstSignIndex, std::string parsedExpression) {
+		std::string operator1 = "";
+		std::string operator2 = "";
+		double oper1;
+		double oper2;
+		for (int i = 0; i < firstSignIndex; i++) {
+			operator1 += parsedExpression.at(i);
+			oper1 = std::atof(operator1.c_str());
+		}
+		for (int i = firstSignIndex + 1; i < parsedExpression.size(); i++) {
+			operator2 += parsedExpression.at(i);
+			oper2 = std::atof(operator2.c_str());
+			
+		}
+		Op* op1 = new Op(oper1);
+		Op* op2 = new Op(oper2);
+		Div* div = new Div(op1, op2);
+		return div;
+}
+
 int main(int argc, char** argv) {
 	
 		std::string expression = argv[1];
@@ -53,7 +113,21 @@ int main(int argc, char** argv) {
 		std::cout << parsedExpression << std::endl;
 
 		if (firstSign1 == '+') {
-			Add* add = addFactory(firstSignIndex, parsedExpression);
+			Add* sum = addFactory(firstSignIndex, parsedExpression);
+			std::cout << sum->evaluate() << std::endl;
+
+		}
+		else if (firstSign1 == '-') {
+			Sub* diff = subFactory(firstSignIndex, parsedExpression);
+			std::cout << diff->evaluate() << std::endl;
+		}
+		else if (firstSign1 == '*') {
+			Mult* prod = multFactory(firstSignIndex, parsedExpression);
+			std::cout << prod->evaluate() << std::endl;
+		}
+		else if (firstSign1 == '/') {
+			Div* fac = divFactory(firstSignIndex, parsedExpression);
+			std::cout << fac->evaluate() << std::endl;
 		}
 		
 	return 0;
